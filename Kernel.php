9f1103734c5789798fefb90e91de4586039003ed
Fabien Potencier <fabien@potencier.org>
2025-02-26 12:01:22 +0100

    Update VERSION for 7.2.4

--- Kernel.php
+++ Kernel.php
@@ -73,12 +73,12 @@ abstract class Kernel implements KernelInterface, RebootableInterface, Terminabl
      */
     private static array $freshCache = [];
 
-    public const VERSION = '7.2.4-DEV';
+    public const VERSION = '7.2.4';
     public const VERSION_ID = 70204;
     public const MAJOR_VERSION = 7;
     public const MINOR_VERSION = 2;
     public const RELEASE_VERSION = 4;
-    public const EXTRA_VERSION = 'DEV';
+    public const EXTRA_VERSION = '';
 
     public const END_OF_MAINTENANCE = '07/2025';
     public const END_OF_LIFE = '07/2025';
