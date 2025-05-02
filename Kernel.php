983ca05eec6623920d24ec0f1005f487d3734a0c
Fabien Potencier <fabien@potencier.org>
2025-05-02 10:46:38 +0200

    Update VERSION for 6.4.21

--- Kernel.php
+++ Kernel.php
@@ -76,12 +76,12 @@ abstract class Kernel implements KernelInterface, RebootableInterface, Terminabl
      */
     private static array $freshCache = [];
 
-    public const VERSION = '6.4.21-DEV';
+    public const VERSION = '6.4.21';
     public const VERSION_ID = 60421;
     public const MAJOR_VERSION = 6;
     public const MINOR_VERSION = 4;
     public const RELEASE_VERSION = 21;
-    public const EXTRA_VERSION = 'DEV';
+    public const EXTRA_VERSION = '';
 
     public const END_OF_MAINTENANCE = '11/2026';
     public const END_OF_LIFE = '11/2027';
