a73edfc38d6bdc779c8c60f57c5a3ed79706eb84
Fabien Potencier <fabien@potencier.org>
2025-05-29 09:51:04 +0200

    Bump Symfony version to 7.3.1

--- Kernel.php
+++ Kernel.php
@@ -73,12 +73,12 @@ abstract class Kernel implements KernelInterface, RebootableInterface, Terminabl
      */
     private static array $freshCache = [];
 
-    public const VERSION = '7.3.0';
-    public const VERSION_ID = 70300;
+    public const VERSION = '7.3.1-DEV';
+    public const VERSION_ID = 70301;
     public const MAJOR_VERSION = 7;
     public const MINOR_VERSION = 3;
-    public const RELEASE_VERSION = 0;
-    public const EXTRA_VERSION = '';
+    public const RELEASE_VERSION = 1;
+    public const EXTRA_VERSION = 'DEV';
 
     public const END_OF_MAINTENANCE = '05/2025';
     public const END_OF_LIFE = '01/2026';
