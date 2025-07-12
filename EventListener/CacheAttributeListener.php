add24c02133cc88b67991abda57efba90c7481f8
Gregor Harlan <mail@gh01.de>
2025-07-12 15:55:19 +0200

    optimize `in_array` calls

--- EventListener/CacheAttributeListener.php
+++ EventListener/CacheAttributeListener.php
@@ -102,7 +102,7 @@ class CacheAttributeListener implements EventSubscriberInterface
         $response = $event->getResponse();
 
         // http://tools.ietf.org/html/draft-ietf-httpbis-p4-conditional-12#section-3.1
-        if (!\in_array($response->getStatusCode(), [200, 203, 300, 301, 302, 304, 404, 410])) {
+        if (!\in_array($response->getStatusCode(), [200, 203, 300, 301, 302, 304, 404, 410], true)) {
             unset($this->lastModified[$request]);
             unset($this->etags[$request]);
 
