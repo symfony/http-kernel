e65986f2057faa18c9c63bf621ae1ac0a9c116ff
Fabien Potencier <fabien@potencier.org>
2025-11-12 12:32:01 +0100

    Bump Symfony version to 6.4.30

--- Kernel.php
+++ Kernel.php
@@ -76,12 +76,12 @@ abstract class Kernel implements KernelInterface, RebootableInterface, Terminabl
      */
     private static array $freshCache = [];
 
-    public const VERSION = '6.4.29';
-    public const VERSION_ID = 60429;
+    public const VERSION = '6.4.30-DEV';
+    public const VERSION_ID = 60430;
     public const MAJOR_VERSION = 6;
     public const MINOR_VERSION = 4;
-    public const RELEASE_VERSION = 29;
-    public const EXTRA_VERSION = '';
+    public const RELEASE_VERSION = 30;
+    public const EXTRA_VERSION = 'DEV';
 
     public const END_OF_MAINTENANCE = '11/2026';
     public const END_OF_LIFE = '11/2027';
