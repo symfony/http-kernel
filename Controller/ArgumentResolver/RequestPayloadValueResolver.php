f4d7834e97c80c8a8823efa196bd4eee5804e355
Nicolas Grekas <nicolas.grekas@gmail.com>
2025-11-20 13:31:22 +0100

    fix merge

--- Controller/ArgumentResolver/RequestPayloadValueResolver.php
+++ Controller/ArgumentResolver/RequestPayloadValueResolver.php
@@ -212,7 +212,7 @@ class RequestPayloadValueResolver implements ValueResolverInterface, EventSubscr
         }
 
         if (\is_array($data)) {
-            return $this->serializer->denormalize($data, $type, 'csv', $attribute->serializationContext + self::CONTEXT_DENORMALIZE);
+            return $this->serializer->denormalize($data, $type, 'csv', $attribute->serializationContext + self::CONTEXT_DENORMALIZE + ('form' === $format ? ['filter_bool' => true] : []));
         }
 
         if ('form' === $format) {
