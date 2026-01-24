c573fca3db08623a76066691b5c1e67cad857c71
Fabien Potencier <fabien@potencier.org>
2026-01-24 18:51:44 +0100

    Update VERSION for 7.3.10

--- Kernel.php
+++ Kernel.php
@@ -74,12 +74,12 @@ abstract class Kernel implements KernelInterface, RebootableInterface, Terminabl
      */
     private static array $freshCache = [];
 
-    public const VERSION = '7.3.10-DEV';
+    public const VERSION = '7.3.10';
     public const VERSION_ID = 70310;
     public const MAJOR_VERSION = 7;
     public const MINOR_VERSION = 3;
     public const RELEASE_VERSION = 10;
-    public const EXTRA_VERSION = 'DEV';
+    public const EXTRA_VERSION = '';
 
     public const END_OF_MAINTENANCE = '01/2026';
     public const END_OF_LIFE = '01/2026';
